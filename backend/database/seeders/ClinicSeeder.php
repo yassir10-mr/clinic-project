bd6eb28b020baf739511763c3fd2f3369edc24f3
raed50 <raidelbouazzati14@gmail.com>
2026-05-22 01:52:33 +0100

    database: config seeder et bdd

--- backend/database/seeders/ClinicSeeder.php
+++ backend/database/seeders/ClinicSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Carbon\Carbon; 
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
 
 class ClinicSeeder extends Seeder
 {
@@ -22,22 +23,52 @@ class ClinicSeeder extends Seeder
             ['nom' => 'Mansouri', 'prenom' => 'Nadia', 'specialite' => 'Pédiatre', 'telephone' => '0622334455', 'email' => 'dr.mansouri@medicare.com', 'matricule' => 'MED-002'],
         ]);
 
-        // 3. Ajouter des Infirmiers (Toi !)
+        // 3. Ajouter des Infirmiers
         DB::table('infirmier')->insert([
-            ['nom' => 'El Bouazzati', 'prenom' => 'Raed', 'telephone' => '0655443322', 'service' => 'Urgences'],
-            ['nom' => 'Tazi', 'prenom' => 'Amine', 'telephone' => '0677889900', 'service' => 'Pédiatrie'],
+            [
+                'nom' => 'El Bouazzati', 
+                'prenom' => 'Raed', 
+                'telephone' => '0655443322', 
+                'email' => 'raed@clinic.com', 
+                'mot_de_passe' => Hash::make('password'), 
+                'service' => 'Urgences'
+            ],
+            [
+                'nom' => 'Tazi', 
+                'prenom' => 'Amine', 
+                'telephone' => '0677889900', 
+                'email' => 'amine@clinic.com',
+                'mot_de_passe' => Hash::make('password'),
+                'service' => 'Pédiatrie'
+            ],
         ]);
 
         // 4. Ajouter des Patients
         $patient1 = DB::table('patient')->insertGetId([
-            'nom' => 'Dupont', 'prenom' => 'Jean', 'date_naissance' => '1985-05-12', 'sexe' => 'Homme', 'adresse' => 'Rabat', 'telephone' => '0601020304', 'email' => 'jean.dupont@email.com', 'groupe_sanguin' => 'O+'
+            'nom' => 'Dupont', 
+            'prenom' => 'Jean', 
+            'date_naissance' => '1985-05-12', 
+            'sexe' => 'Homme', 
+            'adresse' => 'Rabat', 
+            'telephone' => '0601020304', 
+            'email' => 'jean.dupont@email.com', 
+            'groupe_sanguin' => 'O+',
+            'mot_de_passe' => Hash::make('password')
         ]);
 
         $patient2 = DB::table('patient')->insertGetId([
-            'nom' => 'Chraibi', 'prenom' => 'Meryem', 'date_naissance' => '1992-11-23', 'sexe' => 'Femme', 'adresse' => 'Casablanca', 'telephone' => '0612345678', 'email' => 'meryem.c@email.com', 'groupe_sanguin' => 'A+'
+            'nom' => 'Chraibi', 
+            'prenom' => 'Meryem', 
+            'date_naissance' => '1992-11-23', 
+            'sexe' => 'Femme', 
+            'adresse' => 'Casablanca', 
+            'telephone' => '0612345678', 
+            'email' => 'meryem.c@email.com', 
+            'groupe_sanguin' => 'A+',
+            'mot_de_passe' => Hash::make('password')
         ]);
 
-        // 5. Ajouter des Rendez-vous POUR AUJOURD'HUI (Pour que ton module Infirmier marche !)
+        // 5. Ajouter des Rendez-vous POUR AUJOURD'HUI
         DB::table('rendez_vous')->insert([
             [
                 'id_patient' => $patient1, 'id_medecin' => 1, 'id_secretaire' => 1, 
